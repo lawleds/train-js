b7cb5fc82190ed4b06fc081489d9e287cfe2e5f5
lawleds <lawleds@gmail.com>
2020-06-11 17:54:30 +0300

    Template engine and css/html files included

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static("public"));
+app.set("views", "views"); //first argument is express option, second one is folder name
+app.set("view engine", "ejs");
+
 app.get("/", function (req, res) {
-  res.send("Welcome");
+  res.render("home-guest");
 });
 
 app.listen(3000);
-
-
